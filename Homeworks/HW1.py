da2789d137d133b41ade8e448255f638cefe171f
YUSUF KIZILKAYA <78799491+kzlkayayusuf@users.noreply.github.com>
2021-03-11 21:47:20 +0300

    Add files via upload

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,21 @@
-#Explain your work
 
-#Question 1
-for x in range(a):
-	print(a)
+"""
+Created on Thu Mar 11 12:55:41 2021
+
+@author: kzlkayayusuf
+"""
+
+oddNumbersList = list(range(1,10,2))
+evenNumbersList = list(range(0,11,2))
+oddNumbersList.extend(evenNumbersList)
+oddNumbersList.sort()
+
+
+newList=[]
+
+for i in oddNumbersList:
+    number = oddNumbersList[i]*2
+    newList.append(number)
+    print(f"Yeni listenin {i}.inci elemanı {newList[i]}'nin türü: {type(newList[i])}")
+    
+
